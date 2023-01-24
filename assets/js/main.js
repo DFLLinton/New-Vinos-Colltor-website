bee3ebf6385eb0fc97c937c56c31efc11bec87c5
DFLLinton <dominiclinton@gmail.com>
2023-01-24 10:30:49 +0000

    removing old process

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,19 +95,19 @@
 
 })(jQuery);
 
-function makeSpanish(pagestring){
-	if(pagestring == 'index'){
-		document.getElementById("menu").innerHTML = "Menú"
-		document.getElementById("menuitem1").innerHTML = "La Masía"
-		document.getElementById("menuitem2").innerHTML = "El Viñedo"
-		document.getElementById("menuitem3").innerHTML = "La Yurta"
-		document.getElementById("menuitem4").innerHTML = "Destino Turistico"
-		document.getElementById("menuitem5").innerHTML = "Contacto"
-		document.getElementById("menuitem6").innerHTML = "Idiomas"
-		document.getElementById("menuitem7").innerHTML = "Inglés"
-		document.getElementById("menuitem8").innerHTML = "Español"
-		document.getElementById("menuitem9").innerHTML = "Catalán"
-		document.getElementById("menuitem10").innerHTML = "Reservar"
+// function makeSpanish(pagestring){
+// 	if(pagestring == 'index'){
+// 		document.getElementById("menu").innerHTML = "Menú"
+// 		document.getElementById("menuitem1").innerHTML = "La Masía"
+// 		document.getElementById("menuitem2").innerHTML = "El Viñedo"
+// 		document.getElementById("menuitem3").innerHTML = "La Yurta"
+// 		document.getElementById("menuitem4").innerHTML = "Destino Turistico"
+// 		document.getElementById("menuitem5").innerHTML = "Contacto"
+// 		document.getElementById("menuitem6").innerHTML = "Idiomas"
+// 		document.getElementById("menuitem7").innerHTML = "Inglés"
+// 		document.getElementById("menuitem8").innerHTML = "Español"
+// 		document.getElementById("menuitem9").innerHTML = "Catalán"
+// 		document.getElementById("menuitem10").innerHTML = "Reservar"
 
 
 
@@ -117,74 +117,74 @@ function makeSpanish(pagestring){
 
 
 
-		document.getElementById("subtitle1").innerHTML = "Esto es Colltor, un viñedo, masía y destino turístico"
-		document.getElementById("button1").innerHTML = "Book aqui"
-		document.getElementById("header1").innerHTML = "Esto es muy bien"
-		document.getElementById("para1").innerHTML = "Esto es muy bien y creo que es el mejor"
-		document.getElementById("header2").innerHTML = "Esto es muy bien y creo que es el mejor"
-		document.getElementById("para2").innerHTML = "Esto es muy bien y creo que es el mejor"
-		document.getElementById("button2").innerHTML = "Book aqui"
-		document.getElementById("subtitle1").innerHTML = "El Farmhouse"
-		document.getElementById("subtitle2").innerHTML = "Los vineas"
-		document.getElementById("subtitle3").innerHTML = "El yurt"
-		document.getElementById("subpara1").innerHTML = "El yurt"
-		document.getElementById("subpara2").innerHTML = "El yurt"
-		document.getElementById("subpara3").innerHTML = "El yurt"
-		document.getElementById("ctatitle").innerHTML = "Book Aqui"
-		document.getElementById("ctapara").innerHTML = "Reserver tus datos aqui por favor"
-		document.getElementById("button3").innerHTML = "Reservar"
-		document.getElementById("button4").innerHTML = "Air b y b"
-		document.getElementById("button5").innerHTML = "El Booking.com"
-	}if(pagestring == 'yurt'){
-		document.getElementById("title").innerHTML = "El Yurt"
-		document.getElementById("subtitle").innerHTML = "Puedes alojar en el yurt por favor"
-		document.getElementById("para1").innerHTML = "reservar"
-		document.getElementById("para2").innerHTML = "reservar eso por favor"
-		document.getElementById("para3").innerHTML = "reservar aqui por favor"
-		document.getElementById("subtitle2").innerHTML = "los facilities"
+// 		document.getElementById("subtitle1").innerHTML = "Esto es Colltor, un viñedo, masía y destino turístico"
+// 		document.getElementById("button1").innerHTML = "Book aqui"
+// 		document.getElementById("header1").innerHTML = "Esto es muy bien"
+// 		document.getElementById("para1").innerHTML = "Esto es muy bien y creo que es el mejor"
+// 		document.getElementById("header2").innerHTML = "Esto es muy bien y creo que es el mejor"
+// 		document.getElementById("para2").innerHTML = "Esto es muy bien y creo que es el mejor"
+// 		document.getElementById("button2").innerHTML = "Book aqui"
+// 		document.getElementById("subtitle1").innerHTML = "El Farmhouse"
+// 		document.getElementById("subtitle2").innerHTML = "Los vineas"
+// 		document.getElementById("subtitle3").innerHTML = "El yurt"
+// 		document.getElementById("subpara1").innerHTML = "El yurt"
+// 		document.getElementById("subpara2").innerHTML = "El yurt"
+// 		document.getElementById("subpara3").innerHTML = "El yurt"
+// 		document.getElementById("ctatitle").innerHTML = "Book Aqui"
+// 		document.getElementById("ctapara").innerHTML = "Reserver tus datos aqui por favor"
+// 		document.getElementById("button3").innerHTML = "Reservar"
+// 		document.getElementById("button4").innerHTML = "Air b y b"
+// 		document.getElementById("button5").innerHTML = "El Booking.com"
+// 	}if(pagestring == 'yurt'){
+// 		document.getElementById("title").innerHTML = "El Yurt"
+// 		document.getElementById("subtitle").innerHTML = "Puedes alojar en el yurt por favor"
+// 		document.getElementById("para1").innerHTML = "reservar"
+// 		document.getElementById("para2").innerHTML = "reservar eso por favor"
+// 		document.getElementById("para3").innerHTML = "reservar aqui por favor"
+// 		document.getElementById("subtitle2").innerHTML = "los facilities"
 
 
-	}if(pagestring == 'farmhouse'){
-		document.getElementById("title").innerHTML = "El Yurt"
-		document.getElementById("subtitle").innerHTML = "Puedes alojar en el yurt por favor"
-		document.getElementById("para1").innerHTML = "reservar"
-		document.getElementById("para2").innerHTML = "reservar eso por favor"
-		document.getElementById("para3").innerHTML = "reservar aqui por favor"
-		document.getElementById("para4").innerHTML = "reservar aqui por favor"
-		document.getElementById("subtitle2").innerHTML = "los facilities"
+// 	}if(pagestring == 'farmhouse'){
+// 		document.getElementById("title").innerHTML = "El Yurt"
+// 		document.getElementById("subtitle").innerHTML = "Puedes alojar en el yurt por favor"
+// 		document.getElementById("para1").innerHTML = "reservar"
+// 		document.getElementById("para2").innerHTML = "reservar eso por favor"
+// 		document.getElementById("para3").innerHTML = "reservar aqui por favor"
+// 		document.getElementById("para4").innerHTML = "reservar aqui por favor"
+// 		document.getElementById("subtitle2").innerHTML = "los facilities"
 
-	}if(pagestring == 'vineyard'){
-		document.getElementById("title").innerHTML = "El Yurt"
-		document.getElementById("subtitle").innerHTML = "Puedes alojar en el yurt por favor"
-		document.getElementById("para1").innerHTML = "reservar"
-		document.getElementById("para2").innerHTML = "reservar eso por favor"
-		document.getElementById("para3").innerHTML = "reservar aqui por favor"
-		document.getElementById("para4").innerHTML = "reservar aqui por favor"
-		document.getElementById("subtitle2").innerHTML = "los facilities"
-
-
-	}if(pagestring == 'tourist'){
-		document.getElementById("title").innerHTML = "El Yurt"
-		document.getElementById("subtitle").innerHTML = "Puedes alojar en el yurt por favor"
-		document.getElementById("para1").innerHTML = "reservar"
-		document.getElementById("para2").innerHTML = "reservar eso por favor"
-		document.getElementById("para3").innerHTML = "reservar aqui por favor"
-		document.getElementById("subtitle2").innerHTML = "los facilities"
+// 	}if(pagestring == 'vineyard'){
+// 		document.getElementById("title").innerHTML = "El Yurt"
+// 		document.getElementById("subtitle").innerHTML = "Puedes alojar en el yurt por favor"
+// 		document.getElementById("para1").innerHTML = "reservar"
+// 		document.getElementById("para2").innerHTML = "reservar eso por favor"
+// 		document.getElementById("para3").innerHTML = "reservar aqui por favor"
+// 		document.getElementById("para4").innerHTML = "reservar aqui por favor"
+// 		document.getElementById("subtitle2").innerHTML = "los facilities"
 
 
-	}if(pagestring == 'contact'){
-		document.getElementById("title").innerHTML = "El Yurt"
-		document.getElementById("subtitle").innerHTML = "Puedes alojar en el yurt por favor"
-		document.getElementById("button1").innerHTML = "reservar"
-		document.getElementById("button2").innerHTML = "reservar eso por favor"
-		document.getElementById("button3").innerHTML = "reservar aqui por favor"
-		document.getElementById("subtitle2").innerHTML = "los facilities"
-		document.getElementById("para1").innerHTML = "reservar aqui por favor"
-		document.getElementById("para2").innerHTML = "reservar aqui por favor"
-		document.getElementById("para3").innerHTML = "reservar aqui por favor"
-		document.getElementById("para4").innerHTML = "reservar aqui por favor"
-		document.getElementById("para5").innerHTML = "reservar aqui por favor"
+// 	}if(pagestring == 'tourist'){
+// 		document.getElementById("title").innerHTML = "El Yurt"
+// 		document.getElementById("subtitle").innerHTML = "Puedes alojar en el yurt por favor"
+// 		document.getElementById("para1").innerHTML = "reservar"
+// 		document.getElementById("para2").innerHTML = "reservar eso por favor"
+// 		document.getElementById("para3").innerHTML = "reservar aqui por favor"
+// 		document.getElementById("subtitle2").innerHTML = "los facilities"
 
 
-	}else{}
-	}
+// 	}if(pagestring == 'contact'){
+// 		document.getElementById("title").innerHTML = "El Yurt"
+// 		document.getElementById("subtitle").innerHTML = "Puedes alojar en el yurt por favor"
+// 		document.getElementById("button1").innerHTML = "reservar"
+// 		document.getElementById("button2").innerHTML = "reservar eso por favor"
+// 		document.getElementById("button3").innerHTML = "reservar aqui por favor"
+// 		document.getElementById("subtitle2").innerHTML = "los facilities"
+// 		document.getElementById("para1").innerHTML = "reservar aqui por favor"
+// 		document.getElementById("para2").innerHTML = "reservar aqui por favor"
+// 		document.getElementById("para3").innerHTML = "reservar aqui por favor"
+// 		document.getElementById("para4").innerHTML = "reservar aqui por favor"
+// 		document.getElementById("para5").innerHTML = "reservar aqui por favor"
+
+
+// 	}else{}
+// 	}
